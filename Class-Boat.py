0ba4bc3aa0f32c281fec43625159590e0a10ea0b
PharaOFred <frederick.olivier.1@ulaval.ca>
2015-11-20 18:56:09 -0500

    Added comments to complement the codes, made the code easier to read without getting a fucking brain rash.

--- Class-Boat.py
+++ Class-Boat.py
@@ -1,45 +1,82 @@
 class Grid:
-    def __init__(self):
-        grid = ["water"]*100
-        self.grid = grid
 
-    def __getitem__(self, item):
-        return self.grid[item]
-class Grid:
-    def __init__(self):
-        grid = ["water"]*100
-        self.grid = grid
+                   #This class is to set the game's grid, which will be 10x10
+    def __init__(self):     #Constructor operator
 
-    def __getitem__(self, item):
-        return self.grid[item]
+        grid = ["water"]*100    #Our grid's -10x10- i.e a list of 100 elements (tiles)
+        self.grid = grid        #"filled" with water.
 
-    def __setitem__(self, key, value):
-        self.grid[key] = value
+    def __getitem__(self, item):    #Reading operator
 
-class Tile:
-    def __init__(self,m,n,gridtile):
-        self.m = m
-        self.n = n
-        self.gridtile = gridtile
-    def getvalue(self):
-        return self.gridtile[(10*(self.m-1))+(self.n-1)]
-    def setvalue(self,value):
+        return self.grid[item]  #Returns the item at the selected tile this, it is of my
+                                #concern that this operator should take a tuple for argument
+                                # as in i,j = item, where the argument would be (i,j)
+
+    def __setitem__(self, key, value):  # Writing operator
+
+        self.grid[key] = value          #This will set a new string to the targeted position, could be "Aaar, how dare "
+                                        # ye landlubbers hit my ship!" or "Arrr, ye missed ye scallywag!"
+
+
+
+class Tile:                             #Huuuh this class was supposed to inherit from the Grid class...
+                                        #to be continued...
+
+    def __init__(self,m,n,gridtile):    #Constructor for the line position m, the column position n and the grid's tile
+
+        self.m = m                      #Line position of "grid"
+        self.n = n                      #column position of "grid"
+        self.gridtile = gridtile        #No idea what's this for.
+
+
+    def getvalue(self):                                 # Returns the value at the coordinate (m,n), because this is a
+                                                        # list of 100 elements and not 10 lists of 10 elements
+        return self.gridtile[(10*(self.m-1))+(self.n-1)]# we multiply m by 10 to get the appropriate line and only add the column
+                                                        # the minus 1 on both m and n is to avoid having to call
+                                                        #line 0 instead of line 1
+
+
+    def setvalue(self,value):                               # Set the
+
         self.gridtile[(10*(self.m-1))+(self.n-1)] = value
 
+
 class Boat:
+
     def __init__(self,lenght):
+
         self.lenght = lenght
-    def placeboat_horizontaly(self,init_line,init_col,gridboat1):
+
+
+    def PlaceH(self,init_line,init_col,gridboat1):
+
         if 11-self.lenght >= init_col:
+
             for i in range(self.lenght):
+
                 init_tile = Tile(init_line,init_col+i,gridboat1)
                 init_tile.setvalue("boat")
+
         else:
+
             print("ERREUR, MAUVAIS ENDROIT POUR LA LONGUEUR DU BATEAU")
-    def placeboat_verticaly(self,init_line,init_col,gridboat1):
+
+
+    def PlaceV(self,init_line,init_col,gridboat1):
+
         if 11-self.lenght >= init_line:
+
             for i in range(self.lenght):
+
                 init_tile = Tile(init_line+i,init_col,gridboat1)
                 init_tile.setvalue("boat")
+
+
         else:
+
             print("ERREUR, MAUVAIS ENDROIT POUR LA LONGUEUR DU BATEAU")
+
+#Integrated test
+
+G = Grid()
+Sous_marin = Boat(3)
